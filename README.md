1a6aeab8c6917274b29914f38a7f062601c1e399
gabrielademoraes <gabrielademoraes10@gmail.com>
2019-07-01 19:50:32 -0300

    Alterando read me e construindo interface.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Lista6_GabrielaMoraes_LucasPenido
-Lista 6 - Grafos
+## Lista 6 - Grafos
+
+**Alunos:**
+  - Gabriela Chaves de Moraes - 16/0006872
+  - Lucas Penido Antunes - 16/0013143
+
+## Sobre
+<p align="justify"> Esse trabalho compreende um buscador de menor rota entre aeroportos através da construção de um grafo e utilizado o algoritmo da <b>Busca em largura</b>. O grafo foi montado através dos vôos nacionais realizados pela empresa áerea Azul disponíveis no dataset <a href="https://www.kaggle.com/ramirobentes/flights-in-brazil">Flights in Brazil.</a></p>
+
+## Executando
+
+### Linux
+
+#### Pelo terminal
+- Para clonar o repositório:
+> $ git clone https://github.com/EDAII/Lista6_GabrielaMoraes_LucasPenido
+
+- Acesse a pasta do projeto:
+> $ cd Lista6_GabrielaMoraes_LucasPenido/
+
+- Para executar o programa:
+> $ python3 main.py
---
+++ interface.py
@@ -0,0 +1,84 @@
+from tkinter import *
+from tkinter import font
+from tkinter import ttk
+from funcoesGrafo import *
+
+class Application:
+    def __init__(self, master=None, listaDeAdjacencia=dict):
+        self.fonte = font.Font(family="Times", size=12)
+        self.master = master
+        self.listaDeAdjacencia = listaDeAdjacencia
+        self.containerTitulo = Frame(master)
+        self.containerTitulo["padx"] = 50
+        self.containerTitulo["pady"] = 20
+        self.containerTitulo.pack()
+        self.titulo = Label(self.containerTitulo, text="Olá! Seja bem vindo ao Busca-Azul!")
+        self.titulo["font"] = ("Times", "20", "bold")
+        self.titulo.pack ()
+
+        self.containerSubtitulo = Frame(master)
+        self.containerSubtitulo["padx"] = 50
+        self.containerSubtitulo["pady"] = 20
+        self.containerSubtitulo.pack()
+        self.titulo = Label(self.containerSubtitulo, text="Aqui você pode encontrar a menor rota que a Azul realiza\n para vôos nacionais")
+        self.titulo["font"] = ("Times", "14")
+        self.titulo.pack ()
+
+        self.containerOrigem = Frame(master)
+        self.containerOrigem["padx"] = 50
+        self.containerOrigem["pady"] = 20
+        self.containerOrigem.pack()
+        self.lblAeroOrigem = Label(self.containerOrigem, text="Aeroporto Origem:", font=self.fonte, width=20)
+        self.lblAeroOrigem.pack(side=LEFT)
+        self.txtAeroOrigem = Entry(self.containerOrigem)
+        self.txtAeroOrigem["width"] = 80
+        self.txtAeroOrigem["font"] = self.fonte
+        self.txtAeroOrigem.pack()
+
+        self.containerDestino = Frame(master)
+        self.containerDestino["padx"] = 50
+        self.containerDestino["pady"] = 20
+        self.containerDestino.pack()
+        self.lblAeroDestino = Label(self.containerDestino, text="Aeroporto Destino:", font=self.fonte, width=20)
+        self.lblAeroDestino.pack(side=LEFT)
+        self.txtAeroDestino = Entry(self.containerDestino)
+        self.txtAeroDestino["width"] = 80
+        self.txtAeroDestino["font"] = self.fonte
+        self.txtAeroDestino.pack()
+        
+        self.ctBtnBuscar = Frame(master)
+        self.ctBtnBuscar["padx"] = 20
+        self.ctBtnBuscar["pady"] = 10
+        self.ctBtnBuscar.pack()
+        self.btnBuscar = Button(self.ctBtnBuscar, text="Encontrar", font=self.fonte, width=12)
+        self.btnBuscar["command"] = self.imprimeInterfaceMenorCaminho
+        self.btnBuscar.pack(side=LEFT)
+
+        self.containerRota = Frame(master)
+        self.containerRota["padx"] = 50
+        self.containerRota["pady"] = 20
+        self.containerRota.pack()
+   
+    def imprimeInterfaceMenorCaminho(self):
+        labels = []
+        origem = self.txtAeroOrigem.get()
+        destino = self.txtAeroDestino.get()
+
+        # print(self.listaDeAdjacencia)
+
+        largura, nivel, pai = BFS(self.listaDeAdjacencia, origem, destino)
+        menor_caminho = imprime_menor_caminho(pai, origem, destino)
+
+        for label in self.containerRota.winfo_children():
+            label.destroy()
+
+        self.lblTitulo = Label(self.containerRota, text="A menor rota seria passando pelos seguintes aeroportos:", font=self.fonte, width=70)
+        self.lblTitulo.pack()
+        for i in range(0, len(menor_caminho)):
+            variable = menor_caminho[i]
+            sample = Label(self.containerRota, text=variable, font=self.fonte)
+            labels.append(sample)
+            sample.pack()
+            # def connect_callback(variable):
+            #     sample.bind('<Enter>', lambda event:print(variable))
+            # connect_callback(variable)
